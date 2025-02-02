8d1b921d60e09db33cdec5eaeb6df52a4e18f244
Lucas Silva <kucasss19@gmail.com>
2025-02-02 10:39:21 -0300

    implementação da interrupção

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ const uint pino_led_verde = 13;
 
 absolute_time_t turn_off_time;
 
+static volatile uint32_t ultimo_tempo = 0; // variavel para debouce
+
 bool ligar_todos_leds = false;  
 bool desativar_leds = false;
 static cor cor_atual_para_apagar = VERMELHO;
@@ -49,18 +51,46 @@ int main()
     }
 }
 
+void gpio_irq_handler(uint gpio, uint32_t events) {
+
+
+    // fazendo o debouce
+    uint32_t tempo_atual = to_us_since_boot(get_absolute_time());
+
+    bool debouce = tempo_atual - ultimo_tempo > 200000;
+
+    if (!debouce) { // caso nao tenha passado o intervalo ideal entre pressionar os botoes
+        return;
+    }
+
+    ultimo_tempo = tempo_atual; // atualizando o tempo
+
+
+    if (desativar_leds == false) {
+
+        gpio_put(pino_led_vermelho, true);
+        gpio_put(pino_led_azul, true);
+        gpio_put(pino_led_verde, true);
+        desativar_leds = true;
+        turn_off_time = make_timeout_time_ms(3000);
+    }
+
+}
+
 
 bool repeating_timer_callback(struct repeating_timer *t) {
-    if (!desativar_leds) {return true;}
+    if ((!desativar_leds) && absolute_time_diff_us(get_absolute_time(), turn_off_time)) {return true;}
 
     switch (cor_atual_para_apagar) {
         case VERMELHO:
             gpio_put(pino_led_vermelho, false);
             cor_atual_para_apagar = AZUL;
+            turn_off_time = make_timeout_time_ms(3000);
             break;
         case AZUL:
             gpio_put(pino_led_azul, false);
             cor_atual_para_apagar = VERDE;
+            turn_off_time = make_timeout_time_ms(3000);
             break;
         case VERDE:
             gpio_put(pino_led_verde, false);
